8d5faa7a4c8a1a6ef641d0cb3c778ae814d246e6
SakumaTakuya <mo-takusan9922@docomo.ne.jp>
2020-07-05 19:34:41 +0900

    first commit

---
+++ match_move.py
@@ -0,0 +1,14 @@
+import argparse
+import cv2
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--min_match_count', required=False, default=10)
+
+def main(args=parser.parse_args()):
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
