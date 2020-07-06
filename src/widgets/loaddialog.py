b2df2022b13c117bba8d75041f1b020ff1907f4a
SakumaTakuya <mo-takusan9922@docomo.ne.jp>
2020-07-06 15:55:05 +0900

    フォーマットに対応

--- src/widgets/loaddialog.py
+++ src/widgets/loaddialog.py
@@ -1,7 +1,8 @@
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, ListProperty
 from kivy.uix.floatlayout import FloatLayout
 
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
+    filters = ListProperty(["*"])
